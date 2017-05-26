b32a0474d11a3f2c2994a8c3f0e1438075aafbbb
Kavish Ghime <kavishghime90@gmail.com>
2017-05-26 13:30:42 -0700

    Life Expentancy first commit, creating the class

---
+++ src/lifeexpectancy/LifeExpectancy.java
@@ -0,0 +1,18 @@
+package lifeexpectancy;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+
+public class LifeExpectancy {
+	
+	UnfoldingMap map;
+	
+	public void setup(){
+		
+	}
+	
+	public void draw(){
+		
+	}
+	
+
+}
