d0a73e4573899176fb2da320b8c6a5814a81da60
nitis <nitis@192.168.2.11>
2017-05-26 19:51:02 -0700

    added and completed the helper method to read the file csv and put the data of countriews an dtheir life expectancies into a Hash map , map

--- src/lifeexpectancy/LifeExpectancy.java
+++ src/lifeexpectancy/LifeExpectancy.java
@@ -1,5 +1,8 @@
 package lifeexpectancy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.utils.MapUtils;
@@ -19,5 +22,30 @@ public class LifeExpectancy extends PApplet {
 		map.draw();
 	}
 	
+	//helper method to read data
+	//from csv file and
+	//store in the map data structure
+	//the helper method is private
+	//it returns the map data structure
+	//the argument passed in this method is the filename
+	private Map<String, Float> loadLifeExpectancyFromCSV(String filename){
+		
+		//first create the map data structure
+		Map<String, Float> lifeExpMap = new HashMap<String, Float>();
+		
+		//string array of data related to each country
+		//each countries data form a row
+		String[] rows = loadStrings(filename);
+		
+		for(String row: rows){
+			
+			String[] column = row.split(",");// not understood
+			
+			if(column.length==6 && !column[5].equals("..")){
+				lifeExpMap.put(column[4], Float.parseFloat(column[5]));
+			}
+		}	
+		return lifeExpMap;	
+	}
 
 }
